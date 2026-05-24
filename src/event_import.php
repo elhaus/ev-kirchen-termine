ac23bfca4bf40dce5c827a76cbb18ac5188ae847
Jan Elhaus <jan@elhaus.net>
2026-05-25 01:13:48 +0200

    move archive creation to activation hook

--- src/event_import.php
+++ src/event_import.php
@@ -11,6 +11,23 @@ function ev_kirchen_termine_import_events_task_plugin_activate() {
     if (!wp_next_scheduled('ev_kirchen_termine_import_events_task')) {
         wp_schedule_event(time(), 'hourly', 'ev_kirchen_termine_import_events_task');
     }
+
+    $check_page_exist = get_page_by_path('events', 'OBJECT', 'page');
+    // Check if the page already exists
+    if(empty($check_page_exist)) {
+        $page_id = wp_insert_post(
+            array(
+            'comment_status' => 'close',
+            'ping_status'    => 'close',
+            'post_title'     => ucwords('Termine'),
+            'post_name'      => strtolower(trim('events')),
+            'post_status'    => 'publish',
+            'post_content'   => '[events_calendar]',
+            'post_type'      => 'page',
+            )
+        );
+    }
+
 }
 
 function ev_kirchen_termine_import_events_task_plugin_deactivation() {
